ff296418cf485dfafc1ccbb94b8b9580327d7bb7
mrvnre <mrvnre24@gmail.com>
2020-04-06 00:42:30 -0500

    Done with Angular-1 Exercise

---
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fav-links',
+  templateUrl: './fav-links.component.html',
+  styleUrls: ['./fav-links.component.css']
+})
+export class FavLinksComponent implements OnInit {
+  favLinks = ['https://www.youtube.com/', 'https://www.seslisozluk.net/']
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
